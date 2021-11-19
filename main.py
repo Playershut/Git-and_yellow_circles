70aa92b20108a433fa125ee7836778684b950545
aminov.ar75 <arturaminov1512@gmail.com>
2021-11-19 17:53:03 +0500

    Need to finish

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Git и желтые окружности')
+        self.btn = QPushButton('Нарисовать круги', self)
+        self.btn.move(70, 150)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+
+        w, h = self.size().width(), self.size().height()
+
+        for i in range(random.randint(2, 15)):
+            x, y = random.randint(w, h)
+            r = random.randint(10, 125)
+            qp.drawEllipse(x, y, x + r, y - r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
